80000b36c0dd22c7c1ef51541d118585ac17d1bf
raedgeorge <raedgeorge2014@gmail.com>
2025-01-12 14:03:38 +0200

    added common CQRS building block

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+
+    // data needed to create a Product
+    public record CreateProductCommand(
+                                        string Name, 
+                                        List<string> Category,
+                                        string Descriptino,
+                                        string ImageFile,
+                                        decimal Price
+                                       ) : IRequest<CreateProductResult>;
+
+
+    // create Product resut -> return created Product Id
+    public record CreateProductResult(Guid Id);
+
+
+    // create product command handler
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Buisness logic to create a Product
+            throw new NotImplementedException();
+        }
+    }
+}
