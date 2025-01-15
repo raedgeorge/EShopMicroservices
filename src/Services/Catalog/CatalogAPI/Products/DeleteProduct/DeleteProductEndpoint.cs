7f94f76052d9a12ef3aea148916f623a19926a6c
raedgeorge <raedgeorge2014@gmail.com>
2025-01-15 21:07:16 +0200

    added UpdateProduct endpoint and handler

---
+++ src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace CatalogAPI.Products.DeleteProduct
+{
+    public class DeleteProductEndpoint
+    {
+    }
+}
---
+++ src/Services/Catalog/CatalogAPI/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace CatalogAPI.Products.UpdateProduct
+{
+
+    public record UpdateProductCommand(
+                                        Guid Id,
+                                        string Name,
+                                        List<string> Category,
+                                        string Description,
+                                        string ImageFile,
+                                        decimal Price
+                                        ) : ICommand<UpdateProductResult>;
+
+    public record UpdateProductResult(bool IsSuccess);
+
+
+
+    internal class UpdateProductCommandHandler (IDocumentSession session) : 
+            ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, 
+                                                      CancellationToken cancellationToken)
+        {
+
+            var productFromDB = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (productFromDB is null)
+            {
+                throw new ProductNotFoundException("Product not found!");
+            }
+
+            productFromDB.Name = command.Name;
+            productFromDB.Description = command.Description;
+            productFromDB.ImageFile = command.ImageFile;
+            productFromDB.Price = command.Price;
+            productFromDB.Category = command.Category;
+
+            session.Update(productFromDB);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+        }
+    }
+}
