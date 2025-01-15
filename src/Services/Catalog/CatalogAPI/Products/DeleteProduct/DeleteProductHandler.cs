6f27fe99d7fbef90c5e1e382355913b078b8137b
raedgeorge <raedgeorge2014@gmail.com>
2025-01-15 22:20:06 +0200

    added DeleteProduct handler and endpoint

--- src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,6 +1,25 @@
-﻿namespace CatalogAPI.Products.DeleteProduct
+﻿
+namespace CatalogAPI.Products.DeleteProduct
 {
-    public class DeleteProductHandler
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+    public record DeleteProductResult(bool IsSuccess);
+
+
+    internal class DeleteProductCommandHandler (IDocumentSession session)
+        : ICommandHandler<DeleteProductCommand, DeleteProductResult>
     {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, 
+                                               CancellationToken cancellationToken)
+        {
+            var productFromDB = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (productFromDB is null) throw new ProductNotFoundException("Product not found!");
+
+            session.Delete(productFromDB);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
     }
 }
