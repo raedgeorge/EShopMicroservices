75144749a5a7ca82769bf9e260d6cf9d841a3320
raedgeorge <raedgeorge2014@gmail.com>
2025-01-16 20:34:57 +0200

    added PipeLine Behavior for Validation

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,4 @@
-﻿
-namespace CatalogAPI.Products.CreateProduct
+﻿namespace CatalogAPI.Products.CreateProduct
 {
 
     // data needed to create a Product
@@ -16,9 +15,24 @@ namespace CatalogAPI.Products.CreateProduct
     public record CreateProductResult(Guid Id);
 
 
+    public class CreateProductCommandValidator 
+            : AbstractValidator<CreateProductCommand>
+    {
+
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+
+    }
+
+
     // create product command handler
-    internal class CreateProductCommandHandler (IDocumentSession session) : 
-            ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler (IDocumentSession session) 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
 
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,12 +1,20 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddCarter();
 
+var assembly = typeof(Program).Assembly;
+
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+
+
+builder.Services.AddValidatorsFromAssembly(assembly);
 
 // configure Marten to connect to PostgreSQL
 builder.Services.AddMarten(options =>
