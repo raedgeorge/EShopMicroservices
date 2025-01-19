8f7c1e59b964cab5d533d5eef4d14233380996b4
raedgeorge <raedgeorge2014@gmail.com>
2025-01-19 09:54:37 +0200

    add Basket CRUD and register services in program.cs

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+    public record GetBasketResult(ShoppingCart ShoppingCart);
+
+    internal class GetBasketQueryHandler (IDocumentSession session) 
+                   : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+
+            return new GetBasketResult(new ShoppingCart("raed abu sada"));
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,8 +2,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.MapCarter();
 
 app.Run();
