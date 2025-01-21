7fb4599598dd7f3f6d6317e0691998e3cb1715ec
raedgeorge <raedgeorge2014@gmail.com>
2025-01-21 20:05:27 +0200

    add cache for Basket.API and health checks

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,55 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBasketRepository(IBasketRepository repository,
+                                        IDistributedCache cache) 
+                 : IBasketRepository
+    {
+
+        public async Task<ShoppingCart> GetBaksket(string username, CancellationToken cancellationToken = default)
+        {
+
+            var cachedBasket = await cache.GetStringAsync(username, cancellationToken);
+
+            if (!string.IsNullOrEmpty(cachedBasket))
+            {
+                Console.WriteLine($"get basket for {username} from cache");
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+            }
+
+
+            var basket = await repository.GetBaksket(username, cancellationToken);
+
+            await cache.SetStringAsync(username, 
+                                       JsonSerializer.Serialize<ShoppingCart>(basket), 
+                                       cancellationToken);
+
+            return basket;
+        }
+
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart shoppingCart, CancellationToken cancellationToken = default)
+        {
+            var basket = await repository.StoreBasket(shoppingCart, cancellationToken);
+
+            await cache.SetStringAsync(shoppingCart.UserName,
+                                       JsonSerializer.Serialize<ShoppingCart>(basket),
+                                       cancellationToken);
+
+            return basket;
+        }
+
+
+        public async Task<bool> DeleteBasket(string username, CancellationToken cancellationToken = default)
+        {
+            await repository.DeleteBasket(username, cancellationToken);
+
+            await cache.RemoveAsync(username, cancellationToken);
+
+            return true;
+        }
+    }
+}
