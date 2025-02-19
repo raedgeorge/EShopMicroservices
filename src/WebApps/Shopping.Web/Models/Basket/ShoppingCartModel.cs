83f4f797c18ee4cddba73a3565cf391748c559fb
raedgeorge <raedgeorge2014@gmail.com>
2025-02-19 19:33:15 +0200

    added web domain models

---
+++ src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Shopping.Web.Models.Basket
+{
+    public class ShoppingCartModel
+    {
+        public string UserName { get; set; } = default!;
+
+        public List<ShoppingCartItemModel> Items { get; set; } = [];
+
+        public decimal TotalPrice => Items.Sum(i => i.Price * i.Quantity);
+    }
+
+    public class ShoppingCartItemModel
+    {
+        public int Quantity { get; set; } = default!;
+
+        public string Color { get; set; } = default!;
+
+        public decimal Price { get; set; } = default!;
+
+        public Guid ProductId { get; set; } = default!;
+
+        public string ProductName { get; set; } = default!;
+    }
+
+    public record GetBasketResponse(ShoppingCartModel Cart);
+
+    public record StoreBasketRequest(ShoppingCartModel Cart);
+
+    public record StoreBasketResponse(string UserName);
+
+    public record DeleteBasketResponse(bool IsSuccess);
+}
